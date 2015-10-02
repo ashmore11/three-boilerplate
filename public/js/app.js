a29cc1f82cb9bf2c9487bdd516d48cf8e2fab358
Scott Ashmore <dev.scottashmore@gmail.com>
2015-10-02 12:04:48 +0100

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -67,14 +67,10 @@
 	  function APP() {
 	    this.resize = __bind(this.resize, this);
 	    this.update = __bind(this.update, this);
-	    var light;
 	    if (Settings.debug) {
 	      Scene.add(new THREE.GridHelper(50, 10));
 	      Scene.add(new THREE.AxisHelper(60));
 	    }
-	    light = new THREE.SpotLight(0xffffff);
-	    light.position.set(-200, 200, 0);
-	    Scene.add(light);
 	    this.view = new View;
 	    RAF.on('tick', this.update);
 	    win.on('resize', this.resize);
@@ -394,7 +390,7 @@
 /* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var Index, RAF, Scene, Settings, randomColor,
+	var Camera, Index, RAF, Scene, Settings,
 	  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
 	Settings = __webpack_require__(1);
@@ -403,59 +399,80 @@
 
 	Scene = __webpack_require__(8);
 
-	randomColor = __webpack_require__(10);
+	Camera = __webpack_require__(7);
 
 	module.exports = Index = (function() {
-	  Index.prototype.count = 20;
-
-	  Index.prototype.radius = 10;
+	  Index.prototype.particleCount = 3000;
 
 	  function Index() {
 	    this.update = __bind(this.update, this);
-	    var balls, center, geometry, i, material, sphere, _i, _j, _ref;
-	    this.group = new THREE.Object3D;
-	    for (i = _i = 0; _i < 20; i = ++_i) {
-	      balls = new THREE.Object3D;
-	      balls.position.x = (i * 30) - 500;
-	      for (i = _j = 0, _ref = this.count; 0 <= _ref ? _j < _ref : _j > _ref; i = 0 <= _ref ? ++_j : --_j) {
-	        geometry = new THREE.OctahedronGeometry(10, 0);
-	        material = new THREE.MeshPhongMaterial({
-	          color: randomColor({
-	            luminosity: 'light'
-	          }),
-	          wireframe: true
-	        });
-	        sphere = new THREE.Mesh(geometry, material);
-	        center = i * (this.radius * 2) - (this.count * this.radius) + this.radius;
-	        sphere.position.set(0, center * 1.5, 0);
-	        balls.add(sphere);
-	      }
-	      this.group.add(balls);
-	    }
-	    Scene.add(this.group);
+	    this.createNebula();
 	    RAF.on('tick', this.update);
 	  }
 
+	  Index.prototype.createStarfield = function() {
+	    var geometry, i, material, options, particle, particles, x, y, z, _i, _ref;
+	    this.starfield = new THREE.Object3D;
+	    Scene.add(this.starfield);
+	    geometry = new THREE.Geometry;
+	    options = {
+	      color: 0xFFFFFF,
+	      size: 2,
+	      map: THREE.ImageUtils.loadTexture('images/particle.png'),
+	      blending: THREE.AdditiveBlending,
+	      transparent: true
+	    };
+	    material = new THREE.PointCloudMaterial(options);
+	    for (i = _i = 0, _ref = this.particleCount; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+	      x = Math.random() * 500 - 250;
+	      y = Math.random() * 500 - 250;
+	      z = Math.random() * 500 - 250;
+	      particle = new THREE.Vector3(x, y, z);
+	      geometry.vertices.push(particle);
+	    }
+	    particles = new THREE.PointCloud(geometry, material);
+	    particles.sortParticles = true;
+	    return this.starfield.add(particles);
+	  };
+
+	  Index.prototype.createNebula = function() {
+	    var geometry, i, images, material, mesh, options, texture, _i;
+	    images = ['images/plasma.jpg', 'images/plasma2.jpg', 'images/plasma3.jpg'];
+	    this.nebula = new THREE.Object3D;
+	    for (i = _i = 0; _i < 50; i = ++_i) {
+	      geometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+	      texture = THREE.ImageUtils.loadTexture(images[Math.floor(Math.random() * 3)]);
+	      texture.minFilter = THREE.LinearFilter;
+	      options = {
+	        blending: THREE.AdditiveBlending,
+	        transparent: true,
+	        side: THREE.DoubleSide,
+	        depthWrite: false,
+	        depthTest: false,
+	        wireframe: true
+	      };
+	      material = new THREE.MeshNormalMaterial(options);
+	      mesh = new THREE.Mesh(geometry, material);
+	      mesh.rotation.set((Math.PI * i) / 360, 0, 0);
+	      this.nebula.add(mesh);
+	    }
+	    return Scene.add(this.nebula);
+	  };
+
 	  Index.prototype.update = function(time) {
-	    var balls, scale, sphere, _i, _len, _ref, _results;
-	    _ref = this.group.children;
+	    var a, plane, v1, v2, _i, _len, _ref, _results;
+	    _ref = this.nebula.children;
 	    _results = [];
 	    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-	      balls = _ref[_i];
-	      _results.push((function() {
-	        var _j, _len1, _ref1, _results1;
-	        _ref1 = balls.children;
-	        _results1 = [];
-	        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-	          sphere = _ref1[_j];
-	          sphere.rotation.x = Math.sin(time / 500);
-	          sphere.rotation.y = Math.sin(time / 500);
-	          sphere.rotation.z = Math.sin(time / 500);
-	          scale = 0.25 * Math.sin(time / 500) + 1.25;
-	          _results1.push(sphere.scale.set(scale, scale, scale));
-	        }
-	        return _results1;
-	      })());
+	      plane = _ref[_i];
+	      v1 = plane.geometry.faces[0].normal;
+	      v1 = v1.clone().applyMatrix4(plane.matrix);
+	      v2 = Camera.position.clone().sub(plane.position).normalize();
+	      a = v1.dot(v2);
+	      if (a < 0) {
+	        a = a * -1;
+	      }
+	      _results.push(plane.material.opacity = a);
 	    }
 	    return _results;
 	  };
@@ -463,404 +480,6 @@
 	  return Index;
 
 	})();
-
-
-/***/ },
-/* 10 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;// randomColor by David Merfield under the MIT license
-	// https://github.com/davidmerfield/randomColor/
-
-	;(function(root, factory) {
-
-	  // Support AMD
-	  if (true) {
-	    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
-
-	  // Support CommonJS
-	  } else if (typeof exports === 'object') {
-	    var randomColor = factory();
-	    
-	    // Support NodeJS & Component, which allow module.exports to be a function
-	    if (typeof module === 'object' && module && module.exports) {
-	      exports = module.exports = randomColor;
-	    }
-	    
-	    // Support CommonJS 1.1.1 spec
-	    exports.randomColor = randomColor;
-	  
-	  // Support vanilla script loading
-	  } else {
-	    root.randomColor = factory();
-	  };
-
-	}(this, function() {
-
-	  // Seed to get repeatable colors
-	  var seed = null;
-
-	  // Shared color dictionary
-	  var colorDictionary = {};
-
-	  // Populate the color dictionary
-	  loadColorBounds();
-
-	  var randomColor = function(options) {
-	    options = options || {};
-	    if (options.seed && !seed) seed = options.seed;
-
-	    var H,S,B;
-
-	    // Check if we need to generate multiple colors
-	    if (options.count != null) {
-
-	      var totalColors = options.count,
-	          colors = [];
-
-	      options.count = null;
-
-	      while (totalColors > colors.length) {
-	        colors.push(randomColor(options));
-	      }
-
-	      options.count = totalColors;
-
-	      //Keep the seed constant between runs. 
-	      if (options.seed) seed = options.seed;
-	      
-	      return colors;
-	    }
-
-	    // First we pick a hue (H)
-	    H = pickHue(options);
-
-	    // Then use H to determine saturation (S)
-	    S = pickSaturation(H, options);
-
-	    // Then use S and H to determine brightness (B).
-	    B = pickBrightness(H, S, options);
-
-	    // Then we return the HSB color in the desired format
-	    return setFormat([H,S,B], options);
-	  };
-
-	  function pickHue (options) {
-
-	    var hueRange = getHueRange(options.hue),
-	        hue = randomWithin(hueRange);
-
-	    // Instead of storing red as two seperate ranges,
-	    // we group them, using negative numbers
-	    if (hue < 0) {hue = 360 + hue}
-
-	    return hue;
-
-	  }
-
-	  function pickSaturation (hue, options) {
-
-	    if (options.luminosity === 'random') {
-	      return randomWithin([0,100]);
-	    }
-
-	    if (options.hue === 'monochrome') {
-	      return 0;
-	    }
-
-	    var saturationRange = getSaturationRange(hue);
-
-	    var sMin = saturationRange[0],
-	        sMax = saturationRange[1];
-
-	    switch (options.luminosity) {
-
-	      case 'bright':
-	        sMin = 55;
-	        break;
-
-	      case 'dark':
-	        sMin = sMax - 10;
-	        break;
-
-	      case 'light':
-	        sMax = 55;
-	        break;
-	   }
-
-	    return randomWithin([sMin, sMax]);
-
-	  }
-
-	  function pickBrightness (H, S, options) {
-
-	    var brightness,
-	        bMin = getMinimumBrightness(H, S),
-	        bMax = 100;
-
-	    switch (options.luminosity) {
-
-	      case 'dark':
-	        bMax = bMin + 20;
-	        break;
-
-	      case 'light':
-	        bMin = (bMax + bMin)/2;
-	        break;
-
-	      case 'random':
-	        bMin = 0;
-	        bMax = 100;
-	        break;
-	    }
-
-	    return randomWithin([bMin, bMax]);
-
-	  }
-
-	  function setFormat (hsv, options) {
-
-	    switch (options.format) {
-
-	      case 'hsvArray':
-	        return hsv;
-
-	      case 'hslArray':
-	        return HSVtoHSL(hsv);
-
-	      case 'hsl':
-	        var hsl = HSVtoHSL(hsv);
-	        return 'hsl('+hsl[0]+', '+hsl[1]+'%, '+hsl[2]+'%)';
-
-	      case 'rgbArray':
-	        return HSVtoRGB(hsv);
-
-	      case 'rgb':
-	        var rgb = HSVtoRGB(hsv);
-	        return 'rgb(' + rgb.join(', ') + ')';
-
-	      default:
-	        return HSVtoHex(hsv);
-	    }
-
-	  }
-
-	  function getMinimumBrightness(H, S) {
-
-	    var lowerBounds = getColorInfo(H).lowerBounds;
-
-	    for (var i = 0; i < lowerBounds.length - 1; i++) {
-
-	      var s1 = lowerBounds[i][0],
-	          v1 = lowerBounds[i][1];
-
-	      var s2 = lowerBounds[i+1][0],
-	          v2 = lowerBounds[i+1][1];
-
-	      if (S >= s1 && S <= s2) {
-
-	         var m = (v2 - v1)/(s2 - s1),
-	             b = v1 - m*s1;
-
-	         return m*S + b;
-	      }
-
-	    }
-
-	    return 0;
-	  }
-
-	  function getHueRange (colorInput) {
-
-	    if (typeof parseInt(colorInput) === 'number') {
-
-	      var number = parseInt(colorInput);
-
-	      if (number < 360 && number > 0) {
-	        return [number, number];
-	      }
-
-	    }
-
-	    if (typeof colorInput === 'string') {
-
-	      if (colorDictionary[colorInput]) {
-	        var color = colorDictionary[colorInput];
-	        if (color.hueRange) {return color.hueRange}
-	      }
-	    }
-
-	    return [0,360];
-
-	  }
-
-	  function getSaturationRange (hue) {
-	    return getColorInfo(hue).saturationRange;
-	  }
-
-	  function getColorInfo (hue) {
-
-	    // Maps red colors to make picking hue easier
-	    if (hue >= 334 && hue <= 360) {
-	      hue-= 360;
-	    }
-
-	    for (var colorName in colorDictionary) {
-	       var color = colorDictionary[colorName];
-	       if (color.hueRange &&
-	           hue >= color.hueRange[0] &&
-	           hue <= color.hueRange[1]) {
-	          return colorDictionary[colorName];
-	       }
-	    } return 'Color not found';
-	  }
-
-	  function randomWithin (range) {
-	    if (seed == null) {
-	      return Math.floor(range[0] + Math.random()*(range[1] + 1 - range[0]));
-	    } else {
-	      //Seeded random algorithm from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
-	      var max = range[1] || 1;
-	      var min = range[0] || 0;
-	      seed = (seed * 9301 + 49297) % 233280;
-	      var rnd = seed / 233280.0;
-	      return Math.floor(min + rnd * (max - min));
-	    }
-	  }
-
-	  function HSVtoHex (hsv){
-
-	    var rgb = HSVtoRGB(hsv);
-
-	    function componentToHex(c) {
-	        var hex = c.toString(16);
-	        return hex.length == 1 ? "0" + hex : hex;
-	    }
-
-	    var hex = "#" + componentToHex(rgb[0]) + componentToHex(rgb[1]) + componentToHex(rgb[2]);
-
-	    return hex;
-
-	  }
-
-	  function defineColor (name, hueRange, lowerBounds) {
-
-	    var sMin = lowerBounds[0][0],
-	        sMax = lowerBounds[lowerBounds.length - 1][0],
-
-	        bMin = lowerBounds[lowerBounds.length - 1][1],
-	        bMax = lowerBounds[0][1];
-
-	    colorDictionary[name] = {
-	      hueRange: hueRange,
-	      lowerBounds: lowerBounds,
-	      saturationRange: [sMin, sMax],
-	      brightnessRange: [bMin, bMax]
-	    };
-
-	  }
-
-	  function loadColorBounds () {
-
-	    defineColor(
-	      'monochrome',
-	      null,
-	      [[0,0],[100,0]]
-	    );
-
-	    defineColor(
-	      'red',
-	      [-26,18],
-	      [[20,100],[30,92],[40,89],[50,85],[60,78],[70,70],[80,60],[90,55],[100,50]]
-	    );
-
-	    defineColor(
-	      'orange',
-	      [19,46],
-	      [[20,100],[30,93],[40,88],[50,86],[60,85],[70,70],[100,70]]
-	    );
-
-	    defineColor(
-	      'yellow',
-	      [47,62],
-	      [[25,100],[40,94],[50,89],[60,86],[70,84],[80,82],[90,80],[100,75]]
-	    );
-
-	    defineColor(
-	      'green',
-	      [63,178],
-	      [[30,100],[40,90],[50,85],[60,81],[70,74],[80,64],[90,50],[100,40]]
-	    );
-
-	    defineColor(
-	      'blue',
-	      [179, 257],
-	      [[20,100],[30,86],[40,80],[50,74],[60,60],[70,52],[80,44],[90,39],[100,35]]
-	    );
-
-	    defineColor(
-	      'purple',
-	      [258, 282],
-	      [[20,100],[30,87],[40,79],[50,70],[60,65],[70,59],[80,52],[90,45],[100,42]]
-	    );
-
-	    defineColor(
-	      'pink',
-	      [283, 334],
-	      [[20,100],[30,90],[40,86],[60,84],[80,80],[90,75],[100,73]]
-	    );
-
-	  }
-
-	  function HSVtoRGB (hsv) {
-
-	    // this doesn't work for the values of 0 and 360
-	    // here's the hacky fix
-	    var h = hsv[0];
-	    if (h === 0) {h = 1}
-	    if (h === 360) {h = 359}
-
-	    // Rebase the h,s,v values
-	    h = h/360;
-	    var s = hsv[1]/100,
-	        v = hsv[2]/100;
-
-	    var h_i = Math.floor(h*6),
-	      f = h * 6 - h_i,
-	      p = v * (1 - s),
-	      q = v * (1 - f*s),
-	      t = v * (1 - (1 - f)*s),
-	      r = 256,
-	      g = 256,
-	      b = 256;
-
-	    switch(h_i) {
-	      case 0: r = v, g = t, b = p;  break;
-	      case 1: r = q, g = v, b = p;  break;
-	      case 2: r = p, g = v, b = t;  break;
-	      case 3: r = p, g = q, b = v;  break;
-	      case 4: r = t, g = p, b = v;  break;
-	      case 5: r = v, g = p, b = q;  break;
-	    }
-	    var result = [Math.floor(r*255), Math.floor(g*255), Math.floor(b*255)];
-	    return result;
-	  }
-
-	  function HSVtoHSL (hsv) {
-	    var h = hsv[0],
-	      s = hsv[1]/100,
-	      v = hsv[2]/100,
-	      k = (2-s)*v;
-
-	    return [
-	      h,
-	      Math.round(s*v / (k<1 ? k : 2-k) * 10000) / 100,
-	      k/2 * 100
-	    ];
-	  }
-
-	  return randomColor;
-	}));
 
 
 /***/ }
--- src/coffee/views/index.coffee
+++ src/coffee/views/index.coffee
@@ -1,11 +1,97 @@
 Settings = require 'settings'
 RAF      = require 'utils/raf'
 Scene    = require 'helpers/scene'
+Camera   = require 'helpers/camera'
 
 module.exports = class Index
 
+  particleCount: 3000
+
   constructor: ->
+
+    # @createStarfield()
+    @createNebula()
 
     RAF.on 'tick', @update
 
+  createStarfield: ->
+
+    @starfield = new THREE.Object3D
+
+    Scene.add @starfield
+
+    geometry = new THREE.Geometry
+
+    options =
+      color       : 0xFFFFFF
+      size        : 2
+      map         : THREE.ImageUtils.loadTexture('images/particle.png')
+      blending    : THREE.AdditiveBlending
+      transparent : true
+
+    material = new THREE.PointCloudMaterial options
+
+    for i in [0...@particleCount]
+
+      x = Math.random() * 500 - 250
+      y = Math.random() * 500 - 250
+      z = Math.random() * 500 - 250
+
+      particle = new THREE.Vector3 x, y, z
+
+      geometry.vertices.push particle
+
+    particles = new THREE.PointCloud geometry, material
+
+    particles.sortParticles = true
+
+    @starfield.add particles
+
+  createNebula: ->
+
+    images = ['images/plasma.jpg','images/plasma2.jpg', 'images/plasma3.jpg']
+
+    @nebula = new THREE.Object3D
+
+    for i in [0...50]
+
+      geometry = new THREE.PlaneGeometry 100, 100, 1, 1
+
+      texture           = THREE.ImageUtils.loadTexture images[Math.floor(Math.random() * 3)]
+      texture.minFilter = THREE.LinearFilter
+
+      options =
+        # map         : texture
+        blending    : THREE.AdditiveBlending
+        transparent : true
+        side        : THREE.DoubleSide
+        depthWrite  : false
+        depthTest   : false
+        wireframe   : true
+      
+      material = new THREE.MeshNormalMaterial options
+      mesh     = new THREE.Mesh geometry, material
+
+      # mesh.rotation.set i * 0.01, i * 0.01, ( Math.PI * i ) / 360
+      mesh.rotation.set ( Math.PI * i ) / 360, 0, 0
+
+      @nebula.add mesh
+
+    Scene.add @nebula
+
   update: ( time ) =>
+
+    for plane in @nebula.children
+
+      v1 = plane.geometry.faces[0].normal
+      v1 = v1.clone().applyMatrix4( plane.matrix )
+      v2 = Camera.position.clone().sub( plane.position ).normalize()
+      a  = v1.dot v2
+
+      if a < 0 then a = a * -1
+
+      plane.material.opacity = a
+
+      # plane.rotation.x += 0.01
+      # plane.rotation.y += 0.01
+
